db7345051e10a732436eabc5c4112459f63f8c8c
maxtomCMU <pyin2@andrew.cmu.edu>
2022-08-27 18:50:55 -0400

    delete submodules

--- scripts/install_client.sh
+++ scripts/install_client.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 # Install MrExplorer
-
-# Install BioSLAM
+git clone https://github.com/MetaSLAM/Multi-Agent-Simulation.git simulation
+git clone https://github.com/fkie/multimaster_fkie.git multimaster
+git clone https://github.com/MetaSLAM/MrExplorer.git mrexplorer
+git clone https://github.com/MetaSLAM/fakemerge.git fakemerge
